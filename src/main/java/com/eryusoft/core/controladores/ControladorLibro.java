8663e41f58d5bef5ca3722a56edbbe3790c19205
ericpinera <eepinera.emp@gmail.com>
2019-07-05 17:27:18 -0500

    Controlador Libro

--- src/main/java/com/eryusoft/core/controladores/ControladorLibro.java
+++ src/main/java/com/eryusoft/core/controladores/ControladorLibro.java
@@ -1,10 +1,16 @@
 package com.eryusoft.core.controladores;
 
+import java.net.URI;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.eryusoft.core.modelo.Libro;
 import com.eryusoft.core.servicios.LibroServicio;
@@ -31,6 +37,17 @@ public class ControladorLibro
 		return libroServicio.buscar(isbn)
 				.map(ResponseEntity::ok)
 				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Libro> crear(@RequestBody Libro libro,
+						UriComponentsBuilder uriBuilder)
+	{
+		Libro nuevoLibro = libroServicio.crearLibro(libro);
+		
+		URI nuevaURILibro = uriBuilder.path("/libros/{isbn}").build(nuevoLibro.getIsbn());	
+		
+		return ResponseEntity.created(nuevaURILibro).body(nuevoLibro);
 	}
 	
 	
